3e743c2012c01036c8881614aceabc04e88f9246
Jonathan A. Schweder <jonathanschweder@gmail.com>
2018-05-28 19:32:48 -0400

    TestWatcherdDir

--- watcher_test.go
+++ watcher_test.go
@@ -32,6 +32,29 @@ func TestWriteSingleWatchedFile(t *testing.T) {
 	}
 }
 
+func TestWatchedDir(t *testing.T) {
+	tmp1, _ := ioutil.TempFile("/tmp", "watcher_")
+	tmp2, _ := ioutil.TempFile("/tmp", "watcher_")
+	cmd := fmt.Sprintf("echo 'bar' > %s", tmp2.Name())
+
+	t.Logf("Watching %s", tmp1.Name())
+	go Watch(cmd, []string{"/tmp"})
+	time.Sleep(1 * time.Second)
+
+	t.Logf("Writing to %s", tmp1.Name())
+	ioutil.WriteFile(tmp1.Name(), []byte("foo"), 0666)
+	time.Sleep(1 * time.Second)
+
+	t.Logf("Reading to %s", tmp2.Name())
+	content, _ := ioutil.ReadFile(tmp2.Name())
+
+	cmp := strings.TrimSpace(string(content))
+
+	if cmp != "bar" {
+		t.Errorf("Fail when watching a single created file, expected 'bar' found '%s'", content)
+	}
+}
+
 func TestCreationInsideWatchedDir(t *testing.T) {
 	tmp1, _ := ioutil.TempFile("/tmp", "watcher_")
 	tmp2, _ := ioutil.TempFile("/tmp", "watcher_")
