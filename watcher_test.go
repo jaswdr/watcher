b6f8bb457450518b2d0e78a6c611df636cab4a4c
Jonathan A. Schweder <jonathanschweder@gmail.com>
2018-05-28 19:26:22 -0400

    added TestCreationInsideWatchedDir

--- watcher_test.go
+++ watcher_test.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 	"testing"
 	"time"
@@ -28,5 +29,30 @@ func TestWriteSingleWatchedFile(t *testing.T) {
 
 	if cmp != "bar" {
 		t.Errorf("Fail when watching a single file, expected 'bar' found '%s'", content)
+	}
+}
+
+func TestCreationInsideWatchedDir(t *testing.T) {
+	tmp1, _ := ioutil.TempFile("/tmp", "watcher_")
+	tmp2, _ := ioutil.TempFile("/tmp", "watcher_")
+	cmd := fmt.Sprintf("echo 'bar' > %s", tmp2.Name())
+
+	os.Remove(tmp1.Name())
+
+	t.Logf("Watching %s", tmp1.Name())
+	go Watch(cmd, []string{"/tmp"})
+	time.Sleep(1 * time.Second)
+
+	t.Logf("Writing to %s", tmp1.Name())
+	ioutil.WriteFile(tmp1.Name(), []byte("foo"), 0666)
+	time.Sleep(1 * time.Second)
+
+	t.Logf("Reading to %s", tmp2.Name())
+	content, _ := ioutil.ReadFile(tmp2.Name())
+
+	cmp := strings.TrimSpace(string(content))
+
+	if cmp != "bar" {
+		t.Errorf("Fail when watching a single created file, expected 'bar' found '%s'", content)
 	}
 }
